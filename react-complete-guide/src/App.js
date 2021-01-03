ae2b4f419e8a75df39beb37e4c981f91c53fc5c5
xhieu2206 <xhieu94@gmail.com>
2021-01-03 14:07:38 +0700

    Done 46

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,16 +8,41 @@ class App extends Component {
       { name: 'Xuân Hiếu', age: 26 },
       { name: 'Max', age: 30 },
       { name: 'Quân', age: 31 },
-    ]
+    ],
+    otherState: 'Some other state'
   }
+
+  switchNameHandler = (newName) => { // arrow function để lexical con trỏ this
+    // DONT DO THIS: this.state.persons[0].name = 'Nguyễn Xuân Hiếu'; // React sẽ không render lại DOM nó "không biết" rằng chúng ta đã thay đổi state của component
+    this.setState({
+      persons: [
+        { name: newName, age: 26 },
+        { name: 'Max', age: 30 },
+        { name: 'Quân', age: 31 },
+      ]
+    })
+  }
+
+  // () => this.switchNameHandler() sẽ là () => { return this.switchNameHandler() }, return ra một function call
   render() {
     return (
       <div className="App">
         <h1>Hello Xuân Hiếu</h1>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} ></Person>
+        <button onClick={() => this.switchNameHandler('Nguyễn Xuân Hiếu')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} 
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          clickName={this.switchNameHandler.bind(this, 'Maximilian')}>
+            My hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} >
+        </Person>
       </div>
     );
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const person = (props) => { // tên param ở đây là up to you, tuy nhiên nên để là props, đây chính là object có các attribute tương ứng là name của các attribute chúng ta truyền vào lúc gọi components
   // đây là jsx syntax, chúng ta cần import React module để sử dụng
   // props.children ở đây sẽ là bất cứ elements nào nằm giữa opening và closing tag, không chỉ là text mà có thể là các complex HTML
   return (
     <div>
-      <p>I'm a {props.name} and I'm {props.age} years old</p>
+      <p onClick={props.clickName}>I'm a {props.name} and I'm {props.age} years old</p>
       <p>{props.children}</p>
     </div>
   )
