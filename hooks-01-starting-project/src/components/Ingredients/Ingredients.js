f0d894828311983663ec4a84654bca76e49930a3
xhieu2206 <xhieu94@gmail.com>
2021-02-11 18:35:57 +0700

    Done 433

---
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList'
+import Search from './Search';
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients => {
+      return [...prevIngredients, {
+        id: Math.random().toString(),
+        ...ingredient
+      }]
+    });
+  }
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
